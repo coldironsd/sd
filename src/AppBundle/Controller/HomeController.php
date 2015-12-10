180fd9158ad758ecff7a79a89cb7369ecb02298f
socdel <ajsocialdelivery@gmail.com>
2015-12-10 05:07:10 +0000

    security done

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,45 @@
+<?php
+// src/AppBundle/Controller/HomeController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+
+/**
+     * @Route("/home", name="home_route")
+     */
+    public function submitAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+    
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+    
+        return $this->render(
+            'home/home.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+    }
+
+    /**
+     * @Route("/submit_check", name="submit_check")
+     */
+    public function submitCheckAction()
+    {
+        // this controller will not be executed,
+        // as the route is handled by the Security system
+    }
+    
+}
---
+++ src/AppBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+// src/AppBundle/Form/UserType.php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', 'email')
+            ->add('username', 'text')
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            )
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
