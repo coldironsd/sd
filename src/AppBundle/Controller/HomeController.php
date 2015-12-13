448f40312fb4eeeaf583289b6b92fe716ba2ca86
socdel <ajsocialdelivery@gmail.com>
2015-12-13 07:43:30 +0000

    Added request_delivery entity and table

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -1,45 +1,33 @@
 <?php
-// src/AppBundle/Controller/HomeController.php
-namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+// src/AppBundle/Controller/DefaultController.php
+
+// ...
+use AppBundle\Entity\DeliveryRequest;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomeController extends Controller
+// ...
+public function homeAction(Request $request)
 {
+    $dr = new DeliveryRequest();
+    $dr->setName();
+    $dr->setDescription();
+    $dr->setPickupAddr();
+    $dr->setDestAddr();
+    $dr->setCreatedUserId();
+    $dr->setCost();
+    $dr->setDeliveryDate();
+    $dr->setLastUpdated();
+    $dr->setDeliverUserId();
 
-/**
-     * @Route("/home", name="home_route")
-     */
-    public function submitAction(Request $request)
-    {
-        $authenticationUtils = $this->get('security.authentication_utils');
+    $em = $this->getDoctrine()->getManager();
 
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-    
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-    
-        return $this->render(
-            'home/home.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $lastUsername,
-                'error'         => $error,
-            )
-        );
-    }
+    $em->persist($dr);
+    $em->flush();
 
-    /**
-     * @Route("/submit_check", name="submit_check")
-     */
-    public function submitCheckAction()
-    {
-        // this controller will not be executed,
-        // as the route is handled by the Security system
-    }
+    return new Response('Created delivery request id '.$dr->getId());
     
+    // return $this->render(
+    //         'home/home.html.twig'
+    //     );
 }
